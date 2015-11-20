2d60c1449f70ab42c39e6aa36cdf37dcb8ed6ce8
fizk <einar@99designs.com>
2015-11-20 18:16:20 +1100

    Adding CV
    Styling the components
    
    Removing css 'cause thei are auto-generated
    
    Updating Gulp file
    
    Making fure that the css files available before vulcanize runs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/**
+ * Home Page
+ */
+router.get('/', function(request, response, next) {
+    response.render('index', { title: 'Express' });
+});
+
+/**
+ * CV Page
+ */
+router.get('/cv', function(request, response, next) {
+    response.render('index', { title: 'Express' });
 });
 
 module.exports = router;
