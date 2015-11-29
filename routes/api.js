a04e11b1e4bf735cc4d6bcb8ba5bac6bc1fb0493
fizk <einar@99designs.com>
2015-11-30 07:48:42 +1100

    Adding entries

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,22 @@ var router = express.Router();
 var fs = require('fs');
 
 var cvFilePath = './data/cv.json';
+var entriesFilePath = './data/entries.json';
 
 /**
  * CV endpoint
  */
 router.get('/cv', function(request, response, next) {
     fs.readFile(cvFilePath, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        response.json(JSON.parse(data));
+    });
+});
+
+router.get('/entries', function(request, response, next) {
+    fs.readFile(entriesFilePath, 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
